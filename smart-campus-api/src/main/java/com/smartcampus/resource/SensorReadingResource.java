b976e9b1f844815f89289e823cb60495b488c275
Dunura Witharama <dunurawitharama@gmail.com>
2026-03-27 15:26:37 +0530

    Implemented POST readings and sensor value update

--- smart-campus-api/src/main/java/com/smartcampus/resource/SensorReadingResource.java
+++ smart-campus-api/src/main/java/com/smartcampus/resource/SensorReadingResource.java
@@ -23,4 +23,23 @@ public class SensorReadingResource {
     public SensorReadingResource getReadingResource(@PathParam("id") String id) {
         return new SensorReadingResource(id);
 }
+    @POST
+public Response addReading(SensorReading reading) {
+
+    if (!DataStore.sensors.containsKey(sensorId)) {
+        return Response.status(404).entity("Sensor not found").build();
+    }
+
+    DataStore.readings
+            .computeIfAbsent(sensorId, k -> new ArrayList<>())
+            .add(reading);
+
+    // update sensor current value
+    DataStore.sensors.get(sensorId)
+            .setCurrentValue(reading.getValue());
+
+    return Response.status(Response.Status.CREATED)
+            .entity(reading)
+            .build();
+}
 }
