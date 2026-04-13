8c1370e405bad4d9384e595b1409624930361b9e
Dunura Witharama <dunurawitharama@gmail.com>
2026-04-13 09:14:18 +0530

    Implemented request and response logging filter

---
+++ smart-campus-api/src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,27 @@
+package com.smartcampus.filter;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        System.out.println("Request: " 
+            + requestContext.getMethod() + " " 
+            + requestContext.getUriInfo().getPath());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+
+        System.out.println("Response Status: " + responseContext.getStatus());
+    }
+}
