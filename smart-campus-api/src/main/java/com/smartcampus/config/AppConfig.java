5d433464b114abae6aeb65834082b8fef6ec039c
Dunura Witharama <dunurawitharama@gmail.com>
2026-04-18 20:40:08 +0530

    Registered JacksonFeature in AppConfig to enable JSON serialization

--- smart-campus-api/src/main/java/com/smartcampus/config/AppConfig.java
+++ smart-campus-api/src/main/java/com/smartcampus/config/AppConfig.java
@@ -1,9 +1,15 @@
 package com.smartcampus.config;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.jackson.JacksonFeature;
 
 public class AppConfig extends ResourceConfig {
+
     public AppConfig() {
+
         packages("com.smartcampus");
+
+        
+        register(JacksonFeature.class);
     }
 }
