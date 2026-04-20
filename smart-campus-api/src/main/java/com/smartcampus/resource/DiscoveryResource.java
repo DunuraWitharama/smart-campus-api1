ecfa93e3a05767e8c57dc02dc3d4224cff37c5e6
Dunura Witharama <dunurawitharama@gmail.com>
2026-04-20 10:05:00 +0530

    fix: adjust discovery endpoint path for servlet mapping

--- smart-campus-api/src/main/java/com/smartcampus/resource/DiscoveryResource.java
+++ smart-campus-api/src/main/java/com/smartcampus/resource/DiscoveryResource.java
@@ -8,7 +8,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/") 
+@Path("/")  
 @Produces(MediaType.APPLICATION_JSON)
 public class DiscoveryResource {
 
